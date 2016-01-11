b7eb173c4cec242be1d701da2f38d0c1868ead87
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-01-11 13:23:08 -0500

    Animating but not stopping? D:

--- germ.py
+++ germ.py
@@ -1,7 +1,30 @@
 # germ.py
 
 class Germ:
-    def __init__(self, canvas, xy, color):
+    def __init__(self, canvas, xy, color, delta):
         self.canvas = canvas
-        self.id = self.canvas.create_oval(0, 0, 10, 10, fill=color)
+        self.id = self.canvas.create_oval(-10 - abs(delta), -10, 11 + abs(delta), 11, fill=color)
         self.canvas.move(self.id, *xy)
+        if delta > 0:
+            self.delta = delta
+            self.start = self.right
+        else:
+            self.delta = -delta
+            self.start = self.left
+
+    def __call__(self):
+        return self.start
+
+    def right(self):
+        xy = self.canvas.coords(self.id)
+        if xy[1] >= self.canvas.winfo_width():
+            return self.left()
+        self.canvas.move(self.id, self.delta, 0)
+        return self.right
+
+    def left(self):
+        xy = self.canvas.coords(self.id)
+        if xy[0] <= 0:
+            return self.right()
+        self.canvas.move(self.id, -self.delta, 0)
+        return self.left
--- main.py
+++ main.py
@@ -7,10 +7,23 @@ import germ
 
 root = Tk()
 root.title("The Hunger Planks")
+root.resizable(0, 0)
 
-canvas = Canvas(root)
+frame = Frame(root, bd=5, relief=SUNKEN)
+frame.pack()
+
+canvas = Canvas(frame, width=500, height=200, bd=0, highlightthickness=0)
 canvas.pack()
 
-germs = [germ.Germ(canvas, (0, 0), "cyan")]
+germs = [germ.Germ(canvas, (100, 100), "cyan", 5),
+         germ.Germ(canvas, (100, 80), "red", -5),
+         germ.Germ(canvas, (100, 120), "green", 1),
+         germ.Germ(canvas, (100, 150), "gold", 20)]
 
-root.mainloop()
+root.update()
+
+while True:
+    for i in range(len(germs)):
+        germs[i] = germs[i]()
+        root.update_idletasks()
+    root.update()
