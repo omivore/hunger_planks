d71a0a4f8854163a8bd1b084b7da493084d65cda
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-01-12 10:08:03 -0500

    Fixed the speeds generation to not choose 0, so that the germ doesn't just freeze in place.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #! python
 # vim.py
 
+speed_limit = 20
+
 from tkinter import *
 import germ
-import random
-import time
+import random, time, itertools
 
 root = Tk()
 root.title("The Hunger Planks")
@@ -16,8 +17,10 @@ canvas.pack()
 germs = [germ.Germ(canvas, (100, 100), "cyan", 3)]
 
 def spawnGerm(event):
-    germ_colors = ["red","blue" ,"green" ,"magenta" ,"purple" ,"yellow", "azure" ,"cyan" , "snow", "lavenderblush" ,"salmon" ]
-    germs.append(germ.Germ(canvas, (random.randint(0,500), random.randint(0,500)), random.choice(germ_colors), random.randint(-32, 32)))
+    germ_colors = ["red", "blue", "green", "magenta", "purple", "yellow", "azure", "cyan", "snow", "lavenderblush", "salmon"]
+    germ_speeds = list(itertools.chain(range(-speed_limit - 1, 0), range(1, speed_limit + 1)))
+
+    germs.append(germ.Germ(canvas, (random.randint(0,500), random.randint(0,500)), random.choice(germ_colors), random.choice(germ_speeds)))
 
 canvas.bind("<Button-1>",spawnGerm)
 
