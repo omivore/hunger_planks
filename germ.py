4e72b19d92a176de476c7a7e92ac43e66a879c1d
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-01-12 21:27:17 -0500

    Fixed the bug where sometimes germs would get stuck on a side and just stutter along one axis of movement (sometimes stuck in a corner and not moving at all).

--- germ.py
+++ germ.py
@@ -6,14 +6,14 @@ class Germ:
 
     germ_colors = ["green", "magenta", "purple", "yellow", "cyan", "snow", "lavenderblush", "salmon"]
     speed_limit = 3
-    germ_speeds = list(itertools.chain(range(-speed_limit - 1, 0), range(1, speed_limit + 1)))
+    germ_speeds = list(itertools.chain(range(-speed_limit, 0), range(1, speed_limit + 1)))
 
     def __init__(self, canvas: "tkinter.canvas", xy: (int, int), color, velocity: (int, int)):
         """
             Creates a germ given the canvas on which to create it, the coordinates, color, and initial velocity.
 
             canvas - the tkinter canvas that the germ is being created on
-            xy - coordinates; location of the germ
+            xy - coordinates; location of the germ. To prevent sticking, must be between 0 and bounding area, _exclusively_.
             color - a tkinter-viable color (can be hex)
             velocity - the initial speeds and directions of the germ
         """
@@ -29,11 +29,28 @@ class Germ:
 
     def move(self):
         """
-            Moves the germ based on its velocity. Bounces off walls. (supposedly :P)
+            Moves the germ based on its velocity. Bounces off walls.
         """
         xy = self.canvas.coords(self.id)
-        if xy[1] <= 0 or xy[3] >= self.canvas.winfo_height(): self.velocity[1] *= -1
-        if xy[2] >= self.canvas.winfo_width() or xy[0] <= 0: self.velocity[0] *= -1
+        if xy[1] <= 0 or xy[3] >= self.canvas.winfo_height():
+            self.velocity[1] *= -1
+
+            # If germ is stuck in the stuttering gap, this will boost it out.
+            if xy[1] + self.velocity[1] <= 0:
+                self.canvas.move(self.id, 0, 0 - xy[1])
+            elif xy[3] + self.velocity[1] >= self.canvas.winfo_height():
+                self.canvas.move(self.id, 0, self.canvas.winfo_height() - xy[3])
+
+        if xy[2] >= self.canvas.winfo_width() or xy[0] <= 0:
+            self.velocity[0] *= -1
+
+            # Same thing here.
+            if xy[0] + self.velocity[0] <= 0:
+                self.canvas.move(self.id, 0 - xy[0], 0)
+            elif xy[2] + self.velocity[1] >= self.canvas.winfo_width():
+                self.canvas.move(self.id, self.canvas.winfo_width() - xy[2], 0)
+
+
         self.canvas.move(self.id, *self.velocity)
 
     @classmethod
@@ -44,7 +61,7 @@ class Germ:
             cls - the current class. Needed to make this a classmethod
             canvas - the tkinter canvas that the germ is being created on
         """
-        xy = (random.randint(0, canvas.winfo_width()), random.randint(0, canvas.winfo_height()))
+        xy = (random.randint(1, canvas.winfo_width()), random.randint(1, canvas.winfo_height()))
         color = random.choice(cls.germ_colors)
         velocity = (random.choice(cls.germ_speeds), random.choice(cls.germ_speeds))
         return cls(canvas, xy, color, velocity)
