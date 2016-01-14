8df0d7bf3fb21e4122abb471d4e297eed0c0f2be
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-01-13 21:59:38 -0500

    Placed the pivots - they move with the germ. Now to get the germ rotating.

--- germ.py
+++ germ.py
@@ -20,13 +20,19 @@ class Germ:
         # 'Adopts' the canvas, and then creates itself on the canvas with the color given.
         # Then moves itself to the given xy coordinate.
         self.canvas = canvas
-        self.id = self.canvas.create_oval(0, 0, 10, 10, fill=color)
-        self.canvas.move(self.id, *xy)
+        self.tag = "str" + str(id(self))    # The tag in tkinter representing all parts of this germ. Appended "str" to the front b/c tags don't work if they don't contain letters for some reason.
+        self.body = self.canvas.create_oval(0, 0, 10, 10, fill=color, tags=self.tag)
+        self.canvas.move(self.body, *xy)
         self.color = color # Saving this mostly just for debugging purposes.
         
         # Determines the starting speed and direction. velocity contains two 'speeds', and the signs
         # is the direction, left or right, or down or up, corresponding to negative or positive.
         self.velocity = list(velocity)
+
+        # Create the pivots.
+        self.pivots = (self.canvas.create_oval(0, 0, 4, 4, fill=color, tag=self.tag), self.canvas.create_oval(0, 0, 4, 4, fill=color, tag=self.tag))
+        self.canvas.move(self.pivots[0], xy[0] - 5, xy[1] - 10)
+        self.canvas.move(self.pivots[1], xy[0] + 11, xy[1] - 10)
 
     @classmethod
     def from_random(cls, canvas: "tkintercanvas"):
@@ -45,27 +51,27 @@ class Germ:
         """
             Moves the germ based on its velocity. Bounces off walls.
         """
-        xy = self.canvas.coords(self.id)
+        xy = self.canvas.coords(self.tag)
         if xy[1] <= 0 or xy[3] >= self.canvas.winfo_height():
             self.velocity[1] *= -1
 
             # If germ is stuck in the stuttering gap, this will boost it out.
             if xy[1] + self.velocity[1] <= 0:
-                self.canvas.move(self.id, 0, 0 - xy[1])
+                self.canvas.move(self.tag, 0, 0 - xy[1])
             elif xy[3] + self.velocity[1] >= self.canvas.winfo_height():
-                self.canvas.move(self.id, 0, self.canvas.winfo_height() - xy[3])
+                self.canvas.move(self.tag, 0, self.canvas.winfo_height() - xy[3])
 
         if xy[2] >= self.canvas.winfo_width() or xy[0] <= 0:
             self.velocity[0] *= -1
 
             # Same thing here.
             if xy[0] + self.velocity[0] <= 0:
-                self.canvas.move(self.id, 0 - xy[0], 0)
+                self.canvas.move(self.tag, 0 - xy[0], 0)
             elif xy[2] + self.velocity[1] >= self.canvas.winfo_width():
-                self.canvas.move(self.id, self.canvas.winfo_width() - xy[2], 0)
+                self.canvas.move(self.tag, self.canvas.winfo_width() - xy[2], 0)
 
 
-        self.canvas.move(self.id, *self.velocity)
+        self.canvas.move(self.tag, *self.velocity)
 
     def __repr__(self):
         """
