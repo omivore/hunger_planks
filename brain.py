498c665f8b79fefd1cbbe3a65bb2f9ebea466b0c
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-01-20 14:03:21 -0500

    Basic two layer neural net example. Eventually will be adapted, I promise.

--- brain.py
+++ brain.py
@@ -15,20 +15,26 @@ y = np.array([[0],
               [0],
               [1],
               [1]])
-np.random.seed(1)
-synapse0 = 2 * np.random.random((3, 1)) - 1
 
-for _ in range(10000):
+np.random.seed(1)
+synapse0 = 2 * np.random.random((3, 4)) - 1
+synapse1 = 2 * np.random.random((4, 1)) - 1
+
+for _ in range(60000):
     # Forward propagation
     layer0 = x
     layer1 = nonlin(np.dot(layer0, synapse0))
+    layer2 = nonlin(np.dot(layer1, synapse1))
 
     # Calculate error
-    layer1_error = y - layer1
+    layer2_error = y - layer2
+    layer2_delta = layer2_error * nonlin(layer2, True)
 
+    layer1_error = layer2_delta.dot(synapse1.T)
     layer1_delta = layer1_error * nonlin(layer1, True)
 
     # Update the weights
+    synapse1 += np.dot(layer1.T, layer2_delta)
     synapse0 += np.dot(layer0.T, layer1_delta)
 
-print(layer1)
+print(layer2)
